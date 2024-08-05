165a56c6b47296903cc1ad6890c1ee2122513648
ReecerTV <r3769.tv@gmail.com>
2024-08-05 15:41:04 +0200

    ...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'file:///home/thutr/repos/aperture/client/dist/',
   plugins: [
     react(),
     legacy()
